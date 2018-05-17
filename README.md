5934d571ac91a8264da1f1c0369d21ac1e6e1803
Konstantin Läufer <klaeufer@gmail.com>
2018-05-17 10:51:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/klaeufer/issue10709-scala.svg?branch=master)](https://travis-ci.org/klaeufer/issue10709-scala)
+
+This issue has been fixed as of Scala 2.12.5.
---
+++ src/test/scala/IteratorMapIsLazy.scala
@@ -0,0 +1,13 @@
+import org.junit.Assert._
+import org.junit.Test
+import org.mockito.Mockito._
+
+import scala.collection.AbstractIterator
+
+class IteratorMapIsLazy {
+
+  @Test def mapIsLazyUsingSpyFrom(): Unit = {
+    val it = spy(Iterator.from(1))
+    verify(it, never).next() // FIXME ***nondeterministic*** NPE on Linux but not MacOS
+  }
+}
